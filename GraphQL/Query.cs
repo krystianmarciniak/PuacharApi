246c488a8f0f4853120b3c569b3649e2c48f5f5c
Krystian Marciniak <krystianmarciniak446@gmail.com>
2026-01-10 00:46:18 +0100

    Add GraphQL endpoint with Hello query

---
+++ GraphQL/Query.cs
@@ -0,0 +1,7 @@
+namespace PuacharApi.GraphQL;
+
+public class Query
+{
+  public string Hello() => "PuacharApi działa ✅";
+  public DateTime Now() => DateTime.Now;
+}
--- obj/Debug/net9.0/PuacharApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/PuacharApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PuacharApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e78867ba04dc8f46358ad33227d6af066d124b95")]
 [assembly: System.Reflection.AssemblyProductAttribute("PuacharApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PuacharApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
